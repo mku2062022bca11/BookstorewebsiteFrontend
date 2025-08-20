405ae6f9062d28bce948f67da78df32ba4250383
mku2062022bca11 <sakthivelk8340@gmail.com>
2025-08-20 22:37:38 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { AuthProvide } from "./context/AuthContext";
+import { useEffect, useState, createContext } from "react";
+import Loading from "./components/Loading";
+
+// Create Speech Context
+export const SpeechContext = createContext();
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [isSpeechEnabled, setIsSpeechEnabled] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <SpeechContext.Provider value={{ isSpeechEnabled, setIsSpeechEnabled }}>
+      <AuthProvide>
+        <Navbar />
+
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+          <Outlet />
+        </main>
+
+        <Footer />
+      </AuthProvide>
+    </SpeechContext.Provider>
+  );
+}
+
+export default App;
